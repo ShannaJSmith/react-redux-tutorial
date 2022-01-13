fe614813da71764eab5a1e890e862298a293fdee
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-13 15:27:22 -0500

    impletement login and logout functions

--- redux-beginner/src/components/Login.js
+++ redux-beginner/src/components/Login.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux' // used for modifying state
+import { login, logout } from '../features/user'
 
 const Login = () => {
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <button>Login</button>
+      <button onClick={() => {
+        dispatch(login({ name: 'inuyasha', age: 50, email: 'inuyasha@gmail.com' }))
+      }}>Login</button>
+      <button onClick={() => {
+        dispatch(logout())
+      }}>Logout</button>
     </div>
   )
 }
